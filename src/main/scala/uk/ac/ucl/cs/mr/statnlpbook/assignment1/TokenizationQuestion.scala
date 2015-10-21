730d6317119cc3d1ced9a9efafd0e4d18e00ee9d
The-Skas <skas@live.co.uk>
2015-10-21 19:25:56 +0100

    Problem 1, half way there.
    Issue would seem that I am fumbling with regex, perhaps I need more practice with it.
    Last error occurring due to : "whack.. quote not being removed'

--- src/main/scala/uk/ac/ucl/cs/mr/statnlpbook/assignment1/TokenizationQuestion.scala
+++ src/main/scala/uk/ac/ucl/cs/mr/statnlpbook/assignment1/TokenizationQuestion.scala
@@ -31,8 +31,33 @@ object TokenizationQuestion {
     val split_if_start_bar_behind ="""(?<=\[BAR\])"""
     val split_start_bar = (s"$split_if_start_bar_ahead|$split_if_start_bar_behind")
 
+    //This should cause the regex to split at '['|'Chorus' when the value 'Chorus' is found and prepending it a '['
+    val split_chorus_block ="""(?=\[Chorus\])"""
+    val split_chorus_left_bracket  = """(?=Chorus\])"""
+    val split_chorus_right_bracket = """(?=\])(?<=Chorus)|(?<=Chorus])"""
+    val split_chorus = s"""$split_chorus_block|$split_chorus_left_bracket|$split_chorus_right_bracket"""
+
+
+
+    val split_verse_block ="""(?=\[Verse \d+\])"""
+                                            //Verse DDD
+    val split_verse_left_bracket  = """(?=Verse \d{1,3}\])"""
+    val split_verse_right_bracket = """(?=\])(?<=Verse \d{1,3})|(?<=Verse \d{1,3}])"""
+    val split_verse = s"""$split_verse_block|$split_verse_left_bracket|$split_verse_right_bracket"""
+    
+    val split_all_brackets = """(?=\[.*?\])"""
+
+    val split_unparsed_contractions = ("""(?i)(?=can't)|(?i)(?<=can't)|(?i)(?=it's)|(?i)(?<=it's)""")
+    val contraction_cant = ("""(?i)(?<=ca)(?=n't)""")
+    val contraction_its =  ("""(?i)(?<=it)(?='s)""")
+    val split_contractions = (s"""$split_unparsed_contractions|$contraction_cant|$contraction_its""")
+
+    val split_punctuation = """(?=\.|\,|\"))"""
+    //val split_chorus = """(?=\[Chorus\])|(?=Chorus\])|(?=\])(?<=Chorus)|(?<=Chorus])"""
     //TODO: Create a 'seperate_regx(str_patten)' method, which returns (?=pattern|?<=pattern)
-    val tokenizer = Tokenizer.fromRegEx(s"\\s|$split_end_bar|$split_start_bar|(?=A)|(?<=A)")
+
+    val in_bracket = """(.{1,3}(?=\]))"""
+    val tokenizer = Tokenizer.fromRegEx(s"\\s|$split_verse|$split_end_bar|$split_start_bar|$split_chorus|$split_contractions|$split_punctuation")
 
     //the result of the tokenization
     val result = Util.words(raw map tokenizer)
@@ -41,9 +66,11 @@ object TokenizationQuestion {
     val gold = Assignment1Util.loadWords(goldFile).toBuffer
 
     //we find the first pair of tokens that don't match
+    val zip_result = result.zip(gold);
     val mismatch = result.zip(gold).find { case (a, b) => a != b }
 
     //Your goal is to make sure that mismatch is None
+    val regex_test =
     mismatch match {
       case None => println("Success!")
       case Some(pair) => println("The following tokens still don't match: " + pair)
