031042ef8d7361f6232214cd414b446c9cf2d71a
The-Skas <skas@live.co.uk>
2015-11-01 18:55:51 +0000

    Bar and endbar are calculated.

--- src/main/scala/uk/ac/ucl/cs/mr/statnlpbook/assignment1/BarQuestion.scala
+++ src/main/scala/uk/ac/ucl/cs/mr/statnlpbook/assignment1/BarQuestion.scala
@@ -47,6 +47,7 @@ object BarQuestion {
 
     //Does not implement Linear
     var count_distance = 0
+
     def linear_probability(word: String, history: Seq[String]): Double = {
       var prob_any = 1.0 / vocab.size
 
@@ -126,10 +127,10 @@ object BarQuestion {
     var alpha:Double = 1.0
     def some = Segmenter
     val states: Map[String, Int] = Map("UNIFORM" -> 0
-      , "LINEAR" -> 1);
+      , "LINEAR" -> 1, "END-LINEAR" -> 2);
 
-    var curr_state:Int = states("UNIFORM")
-
+    var curr_state:Int = states("END-LINEAR")
+    
     def uniform_probability(word: String, history: Seq[String]): Double =  {
       if (vocab(word)) {
         if(word.contentEquals("[BAR]")) {
@@ -137,11 +138,21 @@ object BarQuestion {
           curr_state = states("LINEAR")
           //Double Check
         }else if(history.contains("[BAR]") && !history.contains("[/BAR]")){
+          //Doent work in some cases where bar and not bar are in the history eeep, so wrong atm BUT
+          // this block only should occur when initialized so its fine.
           count_distance = 0
           curr_state = states("LINEAR")
         }
 
-        
+        else if(word.contentEquals("[/BAR]")){
+          count_distance = 0
+          curr_state = states("END-LINEAR")
+        }else if(history.contains("[/BAR]") && !history.contains("[BAR]")){
+          count_distance = 0
+          curr_state = states("END-LINEAR")
+        }
+
+
         return 1.0 / vocab.size
       }
       else {
@@ -162,7 +173,64 @@ object BarQuestion {
     train(train)
     val total_end_bars:Double = this.hashmap_count_bar_end.foldLeft(0)((sum,t) => sum+t._2)
 
-    def index_linear_probability(word: String, history: Seq[String]): Double = {
+    def endbar_index_linear_probability(word: String, history: Seq[String]): Double = {
+      var prob_any = 1.0 / vocab.size
+
+      //Min is to prevent returning a number over 1
+      //if alphas value gets too large
+      val MAXIMUM_PROBABILITY = 1.0
+
+      //****
+      //Hacky way to make sure
+      //'sample' method works as expected
+
+      val bar_index = history.lastIndexOf("[BAR]")
+      val end_bar_index = history.lastIndexOf("[/BAR]")
+
+      val endbar_index_greater_than_bar_index = end_bar_index > bar_index
+
+      if(bar_index != -1 && endbar_index_greater_than_bar_index) {
+        count_distance = (history.size - 1) - end_bar_index
+      } else {
+        count_distance = 0
+        curr_state = states("LINEAR")
+        return bar_index_linear_probability(word, history)
+      }
+      //'sample' hacky
+      //****
+
+      var number_of_bars_at_index: Double = hashmap_count_bar_end.getOrElse(count_distance, 0).toDouble
+
+      var prob_end_over_count: Double = number_of_bars_at_index / total_end_bars
+
+
+      var prob_end_bar = Math.min(MAXIMUM_PROBABILITY,
+        (prob_end_over_count) *alpha)
+
+      if (vocab(word)) {
+        //Change state
+        if (word.contentEquals("[BAR]")) {
+          curr_state = states("LINEAR")
+
+          //Here return Uniform Probability
+          return prob_end_bar
+        }
+        else {
+          //this distance is not to change the prob of current word
+          count_distance += 1
+
+          //Max is to prevent returning a negative number
+          // if alphas value gets too high
+          val MINIMUM_PROBABILITY = 0.0
+          return Math.max( MINIMUM_PROBABILITY , (1.0 - prob_end_bar)* prob_any )
+        }
+
+      }else {
+        return 0.0
+      }
+    }
+
+    def bar_index_linear_probability(word: String, history: Seq[String]): Double = {
         var prob_any = 1.0 / vocab.size
 
         //Min is to prevent returning a number over 1
@@ -182,7 +250,7 @@ object BarQuestion {
           count_distance = (history.size - 1) - bar_index
         } else {
           count_distance = 0
-          curr_state = states("UNIFORM")
+          curr_state = states("END-LINEAR")
           return prob_any
         }
         //'sample' hacky
@@ -199,7 +267,7 @@ object BarQuestion {
         if (vocab(word)) {
           //Change state
           if (word.contentEquals("[/BAR]")) {
-            curr_state = states("UNIFORM")
+            curr_state = states("END-LINEAR")
 
             //Here return Uniform Probability
             return prob_end_bar
@@ -218,6 +286,7 @@ object BarQuestion {
           return 0.0
         }
       }
+
     def train(train: IndexedSeq[String]): Unit =
     {
       //In this case we don't want order
@@ -248,7 +317,10 @@ object BarQuestion {
           return uniform_probability(word, history)
         }
         else if (curr_state == states("LINEAR")) {
-          return index_linear_probability(word, history)
+          return bar_index_linear_probability(word, history)
+        }
+        else if (curr_state == states("END-LINEAR")) {
+          return endbar_index_linear_probability(word, history)
         }
         else {
           return 0.0
